d62b610b06de7b72d65bfdaf6015b1706fba88f7
Siddu Nhavi <siddunhavi@gmail.com>
2026-03-23 18:16:30 +0530

    Login and Signup form  designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import LandingPage from "./LandingPage/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Auth/login";
+import Signup from "./Auth/Signup";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
