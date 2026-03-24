9994b669bd3c5a6510cbe058b6387f615f32799a
Siddu Nhavi <siddunhavi@gmail.com>
2026-03-24 20:54:08 +0530

    improved rendering logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
